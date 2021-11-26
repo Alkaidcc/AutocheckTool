0d38a551558c8e0aa879cedf72b76f417c61b162
Alkaidcc <932214806@qq.com>
2021-11-26 12:06:22 +0800

    优化配置类

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
-import toml
 import tomlkit
 
 class Config:
     def __init__(self,path) -> None:
         self.tomlPath = path
-    def getScanDirFromToml(self):
-        with open(self.tomlPath,'r',encoding='utf-8') as f:
+        self.scanPath,self.xlsxPath = self.ParsePath(self.tomlPath)
+    def ParsePath(self,path):
+        with open(path,'r',encoding='utf-8') as f:
             data = tomlkit.parse(f.read())
         config = data.get("tool",{}).get("config")
         if not config:
             raise ValueError("Cannot find '[tool.config]' in given toml file!")
-        return config.get("path",[])
+        return config.get("ScanPath",[]), config.get("XlsxPath")
