967c5776fe65c8a72afe10366c962d999c09ef87
Alkaidcc <932214806@qq.com>
2022-03-12 14:45:08 +0800

    feature: 从xlsx读取学生信息

---
+++ stu.py
@@ -0,0 +1,6 @@
+
+
+class Stu:
+    def __init__(self, stu_id = '', stu_name = ''):
+        self.stu_id = stu_id
+        self.stu_name = stu_name
--- utils/util.py
+++ utils/util.py
@@ -1,13 +1,18 @@
 import toml
+from typing import List
+from stu import Stu
 from pathlib import Path
+import os
+from openpyxl import load_workbook
 
-
+CONFIG_PATH = './config.toml'
 xlsx_path = None
 scan_path = None
+result: List[Stu] = []
 
 def _init_config_file():
-    Path('./config.toml').touch()
-    _init_config_by_user('./config.toml')
+    Path(CONFIG_PATH).touch()
+    _init_config_by_user(CONFIG_PATH)
 
 def _init_config_by_user(file_path):
     xlsx_path:Path = get_xlsx_path()
@@ -18,6 +23,35 @@ def _init_config_by_user(file_path):
     }
     with open(file_path, 'w') as f:
         toml.dump(config, f)
+
+def _init_stu_info(xlsx_path) -> List[Stu]:
+    stu_id,stu_name = get_data_from_xlsx(xlsx_path)
+    for index,(id,name) in enumerate(zip(stu_id,stu_name)):
+        if index != 0:
+            result.append(Stu(id,name))
+    return result
+
+def get_stu_len():
+    return len(result)
+
+def get_data_from_xlsx(xlsx_path):
+    try:
+        data = load_workbook(xlsx_path)
+        sheet = data.worksheets[0]
+        col1 = []
+        col2 = []
+        for col in sheet['A']:
+            if col.value == '' or col.value is None:
+                break
+            col1.append(col.value)
+        for col in sheet['B']:
+            if col.value == '' or col.value is None:
+                break
+            col2.append(col.value)
+        return col1, col2
+    except FileNotFoundError:
+        print("Not found xlsx file. Please check config file.")
+        os._exit(0)
 
 def xlsx_valid(xlsx_path: Path):
     if xlsx_path.exists():
